5055ac0665ec210564adcc1bf92cf29d3e595064
vashishthask <vashishtha.sk@gmail.com>
2008-08-10 09:57:46 +0000

    Utility methods added to work with PaginationService

--- TheOtherPages/src/net/sf/theotherpages/util/PaginationServiceHttpUtil.java
+++ TheOtherPages/src/net/sf/theotherpages/util/PaginationServiceHttpUtil.java
@@ -3,24 +3,22 @@ package net.sf.theotherpages.util;
 import javax.servlet.http.HttpServletRequest;
 
 import net.sf.theotherpages.business.PaginationCallback;
+import net.sf.theotherpages.cachestore.SessionAwareCacheStore;
 import net.sf.theotherpages.data.PaginationCallbackParams;
 import net.sf.theotherpages.service.PaginationService;
+import net.sf.theotherpages.service.PaginationServiceImpl;
 
 public class PaginationServiceHttpUtil {
 	PaginationService service;
-	
 	HttpServletRequest request;
 	
-	public PaginationServiceHttpUtil(){
-		
-	}
-	public void setRequest(HttpServletRequest request) {
+	public PaginationServiceHttpUtil(HttpServletRequest request, String paginationId,
+			PaginationCallbackParams callbackParams,
+			PaginationCallback pagingCallBack, String prefix) {
 		this.request = request;
+		this.service = new PaginationServiceImpl(new SessionAwareCacheStore(request.getSession(false)), paginationId, callbackParams, pagingCallBack, prefix);
 	}
-	public PaginationServiceHttpUtil(PaginationService paginationService){
-		service = paginationService;
-		
-	}
+
 	public PaginationService getService() {
 		return service;
 	}
@@ -29,30 +27,28 @@ public class PaginationServiceHttpUtil {
 		this.service = service;
 	}
 
-	public void getFirstPage(String paginationId,
-			PaginationCallbackParams callbackParams,
-			PaginationCallback pagingCallBack, String prefix) throws Exception {
-		request.setAttribute("pageData", service.getFirstPage(paginationId, callbackParams, pagingCallBack, prefix));
+	public void getFirstPage() throws Exception {
+		request.setAttribute("pageData", service.getFirstPage());
 	}
 
-	public void getNextPage(String paginationId) throws Exception {
-		request.setAttribute("pageData", service.getNextPage(paginationId));
+	public void getNextPage() throws Exception {
+		request.setAttribute("pageData", service.getNextPage());
 		
 	}
 
-	public void getPreviousPage(String paginationId) throws Exception {
-		request.setAttribute("pageData", service.getPreviousPage(paginationId));
+	public void getPreviousPage() throws Exception {
+		request.setAttribute("pageData", service.getPreviousPage());
 		
 	}
 
-	public void goToLastPage(String paginationId) throws Exception {
-		request.setAttribute("pageData", service.goToLastPage(paginationId));
+	public void goToLastPage() throws Exception {
+		request.setAttribute("pageData", service.goToLastPage());
 		
 	}
 
-	public void goToPage(String paginationId, int goToPageNumber)
+	public void goToPage(int goToPageNumber)
 			throws Exception {
-		request.setAttribute("pageData", service.goToPage(paginationId, goToPageNumber));
+		request.setAttribute("pageData", service.goToPage(goToPageNumber));
 		
 	}
 }
